593f42553ad8e4596cda944c1b8401c947120516
JamieJoao <jojham1994@gmail.com>
2024-03-18 22:18:55 -0500

    fix: add key to article

--- src/app/page.js
+++ src/app/page.js
@@ -27,13 +27,13 @@ export default async function Home() {
 
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map(obj => (
-            <article className="flex flex-col gap-y-6">
+            <article
+              key={obj.id}
+              className="flex flex-col gap-y-6">
               <div className="aspect-video overflow-hidden rounded-lg shadow">
-                <img src={obj.images[0]} />
+                <img src={obj.images[0]} alt='Imagen del post' />
               </div>
-              <div
-                key={obj.id}
-                className="flex max-w-xl flex-col items-start justify-between">
+              <div className="flex max-w-xl flex-col items-start justify-between">
                 <div className="flex items-center gap-x-4 text-xs">
                   <time dateTime="2020-03-16" className="text-gray-500">{obj.publishedAt}</time>
                   <a href="#" className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{obj.category.name}</a>
@@ -50,7 +50,7 @@ export default async function Home() {
                     dangerouslySetInnerHTML={{ __html: mdToHTML(obj.contentShort) + '...' }}></p>
                 </div>
                 <div className="relative mt-8 flex items-center gap-x-4">
-                  <img src="https://scontent.cdninstagram.com/v/t39.30808-6/430061789_18324571699189237_3994467676443688793_n.jpg?stp=dst-jpegr_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE1MTUuaGRyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=101&_nc_ohc=1XkPJpM_TbgAX9_tS8j&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMxOTYzMzMyMDczOTM0Njk2NQ%3D%3D.2-ccb7-5&oh=00_AfCCGxQpnVQzvn3Z7n4Vb57rWW-HbTFzZ9DISk5G9t86kA&oe=65FDF540&_nc_sid=10d13b" alt="" className="h-10 w-10 rounded-full bg-gray-50" />
+                  <img src="https://scontent.cdninstagram.com/v/t39.30808-6/430061789_18324571699189237_3994467676443688793_n.jpg?stp=dst-jpegr_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE1MTUuaGRyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=101&_nc_ohc=1XkPJpM_TbgAX9_tS8j&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMxOTYzMzMyMDczOTM0Njk2NQ%3D%3D.2-ccb7-5&oh=00_AfCCGxQpnVQzvn3Z7n4Vb57rWW-HbTFzZ9DISk5G9t86kA&oe=65FDF540&_nc_sid=10d13b" alt="Imagen del autor" className="h-10 w-10 rounded-full bg-gray-50" />
                   <div className="text-sm leading-6">
                     <p className="font-semibold text-gray-900">
                       <a href="#">
